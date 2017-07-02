4a6a9338b23ef073b22ad3f28a31315b5257334d
Sam Wheat <sam.wheat@outlook.com>
2017-07-02 07:50:37 -0700

    add app telemetry

--- LeaderAnalytics.Web/Startup.cs
+++ LeaderAnalytics.Web/Startup.cs
@@ -58,6 +58,9 @@ namespace LeaderAnalytics.Web
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            services.AddApplicationInsightsTelemetry(Configuration);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -91,6 +94,8 @@ namespace LeaderAnalytics.Web
             ServeFromDirectory(app, env, "node_modules");
 
             
+
+
             //app.UseMvc(routes =>
             //{
             //    routes.MapRoute(
