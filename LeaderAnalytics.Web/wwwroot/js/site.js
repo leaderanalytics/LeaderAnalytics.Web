a964884184b5a31ff4d667b25273292b94820561
Sam <sam.wheat@outlook.com>
2017-08-05 20:43:20 -0700

    Progress

--- LeaderAnalytics.Web/wwwroot/js/site.js
+++ LeaderAnalytics.Web/wwwroot/js/site.js
@@ -9,6 +9,7 @@
 
             // Load home page
             this.LoadContent('/home.html');
+            this.ShowWindowSize();
         },
 
         LoadContent: function (url) {
@@ -21,7 +22,19 @@
                 url: url,
                 dataType: 'html'
             }).done(function (data) { $('#' + id).html(data); });
+        },
+
+        ShowWindowSize:function()
+        {
+            window.addEventListener('resize', function(event){
+                var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+                document.title =  x + " x "+ y;
+            });        
+
+            
         }
+
+
     }
 
     $(window.site.Init())
