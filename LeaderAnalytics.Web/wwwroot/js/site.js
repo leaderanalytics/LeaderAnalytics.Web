c68ed16e49eec1682f43b68a4e5e428ec05f7a24
Sam <sam.wheat@outlook.com>
2017-08-22 08:21:32 -0700

    start work on contact form

--- LeaderAnalytics.Web/wwwroot/js/site.js
+++ LeaderAnalytics.Web/wwwroot/js/site.js
@@ -49,21 +49,24 @@
             return deferred.promise();            
         },
 
-        ShowWindowSize:function()
-        {
+        ShowWindowSize:function()     {
             window.addEventListener('resize', function(event){
                 var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
                 document.title =  x + " x "+ y;
             });        
-
-            
         },
-        ScrollTo:function(elementID)
-        {
+
+        ScrollTo:function(elementID)  {
             var myElement = document.getElementById(elementID);
             var barHeight = $('.app-bar').height();
             var topPos = myElement.offsetTop - barHeight;
             $('html, body').animate({scrollTop:topPos, scrollLeft:0});
+        },
+
+        ShowContactDialog:function() {
+            this.Load('/contactform.html','dialog').then(function() {
+                metroDialog.open('#dialog');                
+            });
         }
     }
 
