db299b3640364f950c51ea471468606fb98ef456
Sam <sam.wheat@outlook.com>
2020-05-27 06:03:50 -0700

    Fix contact links

--- LeaderAnalytics.Web/Program.cs
+++ LeaderAnalytics.Web/Program.cs
@@ -15,7 +15,7 @@ namespace LeaderAnalytics.Web
         public static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-               .WriteTo.File("serilog\\log", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+               .WriteTo.File("..\\..\\serilog\\WEB\\log", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
                .CreateLogger();
             Log.Information("Logger created");
             Log.Information("ConfigureServices started");
